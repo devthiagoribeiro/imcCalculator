317814874b33d9020e6bd308119d97d9e5bd873b
thgribeiro <thiagordlr@gmail.com>
2022-10-08 13:00:29 -0300

    arpimoração do código

--- index.py
+++ index.py
@@ -7,18 +7,18 @@ peso = float(input("Digite seu peso em kg(ex: 76.5):"))
 
 imc = peso/(alt**2)
 
-if (imc <= 18.5):
-    print("{}, de acordo com os cálculos seu indíce de massa corporal corresponde a {:.1f} e você se encotra {}!".format(nome,imc,"abaixo do peso"))
-if ((imc >= 18.6) and (imc <= 24.9)):
-    print("{}, de acordo com os cálculos seu indíce de massa corporal corresponde a {:.1f} e você se encotra {}!".format(nome,imc,"no peso ideal"))
-if ((imc >= 25.0) and (imc <= 29.9)):
-     print("{}, de acordo com os cálculos seu indíce de massa corporal corresponde a {:.1f} e você se encotra {}!".format(nome,imc,"acima do peso"))
-if ((imc >= 30.0) and (imc <= 39.9)):
-    print("{}, de acordo com os cálculos seu indíce de massa corporal corresponde a {:.1f} e você se encotra {}!".format(nome,imc,"com obesidade"))
+if imc < 18.5:
+    print(f"{nome}, de acordo com os cálculos seu indíce de massa corporal corresponde a {imc:.1f} e você se encotra abaixo do peso!")
+elif (imc > 18.5) and (imc < 24.9):
+    print(f"{nome}, de acordo com os cálculos seu indíce de massa corporal corresponde a {imc:.1f} e você se encotra no peso ideal!")
+elif ((imc > 24.9) and (imc < 29.9)):
+     print(f"{nome}, de acordo com os cálculos seu indíce de massa corporal corresponde a {imc:.1f} e você se encotra acima do peso!")
+elif ((imc > 29.9) and (imc < 39.9)):
+    print(f"{nome}, de acordo com os cálculos seu indíce de massa corporal corresponde a {imc:.1f} e você se encotra em estado de obesidade!")
 else:
-    print("{}, de acordo com os cálculos seu indíce de massa corporal corresponde a {:.1f} e você se encotra {}!".format(nome,imc,"com obesidade grave"))
+    print(f"{nome}, de acordo com os cálculos seu indíce de massa corporal corresponde a {imc:.1f} e você se encotra em obesidade morbida!")
 
-#desenvolvida pro Thiago Ribeiro
+#desenvolvido pro Thiago Ribeiro
 
 
     
